df2afa8d79132032f0aa8df29ec52783e5a28eb5
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2023-11-22 16:57:37 +0100

    Update Notification.jsx

--- components/ui/Notification.jsx
+++ components/ui/Notification.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 
-import classes from './notification.module.css';
+import classes from './Notification.module.css';
 
 function Notification(props) {
   const { title, message, status } = props;
